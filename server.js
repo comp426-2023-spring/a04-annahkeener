4afd03c73dcf380ed2351646e3b1a09b4e901648
Anna <ahk@live.unc.edu>
2023-05-07 23:55:23 -0400

    changed req

--- server.js
+++ server.js
@@ -5,54 +5,57 @@ import minimist from "minimist";
 const app = express();
 app.use(express.json());
 
-// Use minimist to process one argument `--port=` on the command line after `node server.js`.
-// Define a const `port` using the argument from the command line. 
-// Make this const default to port 5000 if there is no argument given for `--port`.
-
 const args = minimist(process.argv.slice(2));
 console.log(args);
 const port = (args.port || 5000);
 
 // 3. READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
-    res.json({"message":"Your API works! :))) (200)"});
-	res.status(200);
+
+    res.status(200).json({"message":"200 OK"});
+	
 });
 
 // 4. rps single
 app.get("/app/rpsFunct/", (req, res) => {
-    const shot = rpsFunct();
-    res.status(200).json({shot});
+    
+    const game = rpsFunct();
+    res.status(200).json({game});
 });
 
 // 5. rpsls single
 app.get("/app/rpslsFunct/", (req, res) => {
-    const shot = rpslsFunct();
-    res.status(200).json({shot});
+    
+    const game = rpslsFunct();
+    res.status(200).json({game});
 });
 
 // 6. rps opponent
 app.get('/app/rpsFunct/play/', (req, res, next) => {
-    const shot = rpsFunct(req.params.playerChoice)
-    res.status(200).json({shot})
+    
+    const game = rpsFunct(req.params.playerChoice)
+    res.status(200).json({game})
 })
 
 // 7. rpsls opponent
 app.get('/app/rpslsFunct/play/', (req, res, next) => {
+    
     const game = rpslsFunct(req.params.playerChoice)
-    res.status(200).json({shot})
+    res.status(200).json({game})
 })
 
 // 8. rps single
-app.get('/app/rpslsFunct/play/(rock|paper|scissors)/', (req, res, next) => {
-    const shot = rpsFunct();
-    res.status(200).json({shot})
+app.get('/app/rpsFunct/play/:shot/', (req, res, next) => {
+  
+    const game = rpsFunct(req.body.shot);
+    res.status(200).json({game})
 })
 
 // 9. rpsls
-app.get('/app/rpslsFunct/play/(rock|paper|scissors|lizard|spock)/', (req, res, next) => {
-    const shot = rpsFunct();
-    res.status(200).json({shot})
+app.get('/app/rpslsFunct/play/:shot', (req, res, next) => {
+    
+    const game = rpsFunct(req.body.shot);
+    res.status(200).json({game})
 })
 
 // 10. Default API endpoint that returns 404 Not found for any endpoints that are not defined.
